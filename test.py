aae677b540c5c7ba835f1e80088890969c747160
Thomas Rocheteau <rochetet@3il.fr>
2023-05-13 14:22:58 +0200

    Linked redirections to users

--- test.py
+++ test.py
@@ -7,14 +7,32 @@ p.unlink()
 
 with app.app_context():
     db.create_all()
-    redirection = Redirection(
+    red1 = Redirection(
         url_from = "aaaa",
         url_to = "https://www.google.fr"
     )
-    user = User(
+    db.session.add(red1)
+
+    user1 = User(
         email = "toto@toto.fr",
         password = "totototo"
     )
-    db.session.add(redirection)
-    db.session.add(user)
+    db.session.add(user1)
     db.session.commit()
+
+    red2 = Redirection(
+        user = user1.id,
+        url_from = "bbbb",
+        url_to = "https://www.duckduckgo.com"
+    )
+    db.session.add(red2)
+    red2 = Redirection(
+        user = user1.id,
+        url_from = "cccc",
+        url_to = "https://www.ecosia.com"
+    )
+    db.session.add(red2)
+    db.session.commit()
+
+    red = db.session.execute(db.select(Redirection).where(Redirection.user == user1.id)).scalars()
+    print([r for r in red])

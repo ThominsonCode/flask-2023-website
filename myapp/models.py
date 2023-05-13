f43d74c41c9d21918db954e735f4f702065bc3b4
Thomas Rocheteau <rochetet@3il.fr>
2023-05-13 14:23:18 +0200

    linked redirections to Users

--- myapp/models.py
+++ myapp/models.py
@@ -2,16 +2,20 @@ from datetime import datetime
 from myapp import db
 from flask_login import UserMixin
 
-class Redirection(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url_from = db.Column(db.String(20), unique=True, nullable=False)
-    url_to = db.Column(db.String(120), nullable=False)
-
-    def __repr__(self):
-        return f"Redirection('{self.url_from}' => '{self.url_to}')"
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
+
+
+class Redirection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.ForeignKey(User.id), unique=False, nullable=True)
+    url_from = db.Column(db.String(20), unique=True, nullable=False)
+    url_to = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"Redirection('{self.url_from}' => '{self.url_to}')"
+
   
